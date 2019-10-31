8e535665b364f7af5d7d1b5bfff2bfdb5890b89d
Thomas Sönnerstam <sonnerstam.thomas@hotmail.com>
2019-10-31 19:34:35 +0100

    Major changes to all files

--- hemmaperioder.php
+++ hemmaperioder.php
@@ -1,18 +1,18 @@
 <?php require __DIR__ . '/header.php'; ?>
 
 <section class="hemmaperioder">
-	<div>
-		<h1>Perioder</h1>
-		<p>
-			Augusti är generellt sett varmt, ibland kan det till och med gå upp till 34°C. I augusti är det även fuktigt, så om man gillar fuktigare klimat så är detta en perfekt månad att åka till Barcelona på. Ibland kan det till och med bli för varmt, eftersom det är så pass fuktigt, så man får se till att basta mycket hemma så man vänjer sig vid värmen! Om man ska ta in på hotell, så se därför till att ha en AC!
-			<br><br>Vädret i oktober och november, då jag var där, är fortfarande relativt varmt. Det kan bli allt från 15°C upp till 25°C. Det kanske inte är soligt varje dag, så var beredda på att det kan bli lite regn. Jag var där i nästan tre veckor, och det regnade kanske halva tiden.
-		</p>
-		<img src="hemmaperioder.jpg" alt="">
-		<p>
-			Från november till februari så är det ”vinter” i Barcelona. Då är temperaturen lite lägre, allt från 8°C till 12°C. Under denna period regnar det inte så mycket, så man kan fortfarande ha turen att få sol.
-			<br><br>Kom ihåg att detta endast är <em>riktlinjer</em>, så förvänta er inte att ni ska få sol och värme för att ni åker i oktober, utan detta kan variera från år till år. På senare tid så har vädret i Barcelona gått ifrån dess normer.
-		</p>
-	</div>
+
+	<h1>Perioder</h1>
+	<p>
+		Augusti är generellt sett varmt, ibland kan det till och med gå upp till 34°C. I augusti är det även fuktigt, så om man gillar fuktigare klimat så är detta en perfekt månad att åka till Barcelona på. Ibland kan det till och med bli för varmt, eftersom det är så pass fuktigt, så man får se till att basta mycket hemma så man vänjer sig vid värmen! Om man ska ta in på hotell, så se därför till att ha en AC!
+		<br><br>Vädret i oktober och november, då jag var där, är fortfarande relativt varmt. Det kan bli allt från 15°C upp till 25°C. Det kanske inte är soligt varje dag, så var beredda på att det kan bli lite regn. Jag var där i nästan tre veckor, och det regnade kanske halva tiden.
+	</p>
+	<img src="hemmaperioder.jpg" alt="">
+	<p>
+		Från november till februari så är det ”vinter” i Barcelona. Då är temperaturen lite lägre, allt från 8°C till 12°C. Under denna period regnar det inte så mycket, så man kan fortfarande ha turen att få sol.
+		<br><br>Kom ihåg att detta endast är <em>riktlinjer</em>, så förvänta er inte att ni ska få sol och värme för att ni åker i oktober, utan detta kan variera från år till år. På senare tid så har vädret i Barcelona gått ifrån dess normer.
+	</p>
+
 </section>
 
 <?php require __DIR__ . '/footer.php'; ?>
